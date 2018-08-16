133e7e5312561745a696fc43a4efe67a8ace5aff
Sujay Bawaskar <sujay.bawaskar@coupa.com>
2018-08-16 17:34:16 +0530

    Added edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,36 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+  def new
+    @portfolio_item = Portfolio.new
+  end
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+    respond_to do |format| if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title,:body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 end
+
+
+
+private
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :body)
+  end
