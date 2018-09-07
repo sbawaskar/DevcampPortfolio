8aac3ac5a169b032f8535a3567c45e54e8227292
Sujay Bawaskar <sujay.bawaskar@coupa.com>
2018-09-07 10:01:59 +0530

    Added toggle status functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
   root to: 'pages#home'
 end
