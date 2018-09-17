bff9dca052630fdd1120ca9befc997974354d738
Sujay Bawaskar <sujay.bawaskar@coupa.com>
2018-09-17 17:28:21 +0530

    Implemented defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,5 +8,12 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> {where(:subtitle => "Ruby on rails")}
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x200"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
+
 
 end
