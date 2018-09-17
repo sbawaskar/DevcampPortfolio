cf36702c7db9fc35b783d275761ed6866d303c85
Sujay Bawaskar <sujay.bawaskar@coupa.com>
2018-09-17 18:01:59 +0530

    Added concern

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_082729) do
+ActiveRecord::Schema.define(version: 2018_09_17_120434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_09_10_082729) do
     t.integer "percent_utlized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
